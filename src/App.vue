e1ea872ff22ed25512702670d8cf44af5f4576f3
lavinaaliter <lavina@alitersolutions.com>
2023-03-31 12:54:06 +0530

    add signup page

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <div class="main">
+    <img alt="logo" src="./assets/restaurent-logo.webp" class="logo" />
+    <SignUp />
+  </div>
 </template>
 
 <script>
+import SignUp from "./components/signUp.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-  }
-}
+    SignUp,
+  },
+};
 </script>
 
 <style>
+template {
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.logo {
+  width: 200px;
 }
 </style>
---
+++ src/components/signUp.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="sign">
+    <h1>Sign Up</h1>
+
+    <div class="form">
+      <div class="uname">
+        <input type="text" name="username" v-model="user" class="input" placeholder="Enter Username"/>
+      </div>
+      <div class="email">
+        <input type="email" name="mail" v-model="email" class="input" placeholder="Enter Email" />
+      </div>
+      <div class="passw">
+        <input type="password" name="password" v-model="password" class="input" placeholder="Enter password" />
+      </div>
+      <button v-on:click="Sign()" class="btn">Sign Up</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "SignUp",
+  data() {
+    return {
+      user: "",
+      password: "",
+      email: ""
+    };
+  },
+  methods: {
+    async Sign() {
+        let result= await axios.post ("http://localhost:3000/user" , {
+            username : this.user,
+            email : this.email,
+            password : this.password
+
+        })
+      console.log("data" , result.data) 
+      if (result.status==201) {
+        alert("you have successfully registered !!!!!!!!!!!!!!")
+      }
+      localStorage.setItem ("signup data" , JSON.stringify (result.data))
+    },
+  },
+};
+</script>
+
+<style scoped>
+h1{
+    margin: 10px;
+    padding-bottom: 20px;
+}
+.input{
+    margin: 10px;
+    padding: 10px 15px;
+    font-size: 18px;
+    border-radius: 5px;
+    border: 1px solid black;
+}
+.btn{
+    margin-top: 30px;
+    border: 1px solid black;
+    border-radius: 20px;
+    padding: 10px 20px;
+    font-size: 20px;
+    background-color: black;
+    color: white;
+}
+</style>
